a4a487f735e0132f6aa57b0dbccf9a73a1e45345
sojeongsojeong-park <psj90972@naver.com>
2022-08-03 22:33:27 +0900

    feat: add email and password validation

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -4,6 +4,11 @@ function SignUp() {
   const [idInput, setIdInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
 
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const emailValidation = emailRegex.test(idInput);
+  const passwordValidation = passwordInput.length < 8;
+
   const idChangeHandler = (e) => {
     setIdInput(e.target.value);
   };
@@ -11,24 +16,35 @@ function SignUp() {
     setPasswordInput(e.target.value);
   };
   const signupClickHandler = () => {
-    const data = {
-      email: idInput,
-      password: passwordInput,
-    };
+    if (!emailValidation) {
+      alert("이메일을 확인해 주세요.");
+    } else if (passwordValidation) {
+      alert("비밀번호는 8자 이상이어야 합니다.");
+    }
+    if (emailValidation && !passwordValidation) {
+      try {
+        const data = {
+          email: idInput,
+          password: passwordInput,
+        };
 
-    fetch("http://localhost:8080/users/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.token) localStorage.setItem("token", res.token);
-        if (res.message) alert(res.message);
-        if (res.details) alert(res.details);
-      });
+        fetch("http://localhost:8080/users/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.token) localStorage.setItem("token", res.token);
+            if (res.message) alert(res.message);
+            if (res.details) alert(res.details);
+          });
+      } catch (e) {
+        console.error(e);
+      }
+    }
   };
 
   return (
