55cf6e7b9adcfce09fae415045a7766b70d83569
sojeongsojeong-park <psj90972@naver.com>
2022-08-04 11:14:36 +0900

    feat: change routing structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
+import Nav from "./components/Nav";
+import Todo from "./pages/Todo";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/auth/login' element={<Login />} />
-      <Route path='/auth/signup' element={<SignUp />} />
-    </Routes>
+    <BrowserRouter>
+      <Nav></Nav>
+      <Routes>
+        <Route path='/' element={<Todo />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/auth/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
