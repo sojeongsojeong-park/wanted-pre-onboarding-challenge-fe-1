8266e5ff7d01122ec2123e2b550e3c4a05d4daee
sojeongsojeong-park <psj90972@naver.com>
2022-08-04 23:32:33 +0900

    feat: implement todo detail

--- src/components/TodoDetail.jsx
+++ src/components/TodoDetail.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import UpdateTodo from "./UpdateTodo";
 
-function TodoDetail({ id, token }) {
+function TodoDetail({ id, token, updateTodoHandler }) {
   const [data, setData] = useState({});
+  const [updateClick, setUpdateClick] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -17,14 +19,43 @@ function TodoDetail({ id, token }) {
     })();
   }, [id]);
 
+  const deleteClickHandler = async () => {
+    if (window.confirm("are you sure to delete?")) {
+      await fetch(`http://localhost:8080/todos/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.data === null) alert("deleted!");
+          updateTodoHandler(false, "");
+        });
+    } else {
+      console.log("alive");
+    }
+  };
+  const updateClickHandler = () => {
+    setUpdateClick(true);
+  };
+
   return (
     <>
       <h2>{data.title}</h2>
       <p>{data.content}</p>
       <ul>
-        <li> update </li>
-        <li> delete </li>
+        <li onClick={updateClickHandler}> update </li>
+        <li onClick={deleteClickHandler}> delete </li>
       </ul>
+      {updateClick && (
+        <UpdateTodo
+          id={id}
+          token={token}
+          updateTodoHandler={updateTodoHandler}
+        />
+      )}
     </>
   );
 }
