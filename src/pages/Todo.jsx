e06371086e262559088b80166ae6885e35f44ab4
sojeongsojeong-park <psj90972@naver.com>
2022-08-06 13:13:54 +0900

    style: add Todo style

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CreateTodo from "../components/CreateTodo";
 import TodoDetail from "../components/TodoDetail";
+import styled from "styled-components";
 
 const token = localStorage.getItem("token");
 
@@ -10,6 +11,7 @@ function Todo() {
   const [clickedId, setClickedId] = useState({
     state: false,
     id: "",
+    timing: "",
   });
   useEffect(() => {
     (async () => {
@@ -23,32 +25,34 @@ function Todo() {
         .then((res) => res.json())
         .then((res) => setGetTodo(res.data));
     })();
-  }, [createState, clickedId.state]);
+  }, [createState, clickedId.timing]);
 
-  const addTodoClickHandler = () => {
-    setCreateState(true);
-  };
   const createTodoHandler = (value) => {
     setCreateState(value);
   };
-  const updateTodoHandler = (value, id) => {
-    setClickedId({ state: value, id: id });
+  const updateTodoHandler = (value, id, timing) => {
+    setClickedId({ state: value, id: id, timing: timing });
   };
   const TodoLiClickHandler = (e) => {
-    setClickedId({ state: true, id: e.target.id });
+    setClickedId({ state: true, id: e.target.id, timing: "" });
   };
   return (
-    <div>
-      <button onClick={addTodoClickHandler}> + add todo</button>
-      <ul>
+    <TodoContainer>
+      <AddTodo onClick={() => createTodoHandler(true)}> + add todo</AddTodo>
+      <TodoUl isClicked={clickedId.state}>
         {getTodo.map((data, idx) => {
           return (
-            <li key={idx} id={data.id} onClick={TodoLiClickHandler}>
-              {data.title}
-            </li>
+            <TodoLi
+              key={idx}
+              id={data.id}
+              onClick={TodoLiClickHandler}
+              value={clickedId.id}
+            >
+              ✓ &nbsp; {data.title}
+            </TodoLi>
           );
         })}
-      </ul>
+      </TodoUl>
       {clickedId.state && (
         <TodoDetail
           updateTodoHandler={updateTodoHandler}
@@ -59,8 +63,44 @@ function Todo() {
       {createState && (
         <CreateTodo createTodoHandler={createTodoHandler} token={token} />
       )}
-    </div>
+    </TodoContainer>
   );
 }
 
 export default Todo;
+
+const AddTodo = styled.button`
+  display: block;
+  width: 15rem;
+  height: 3rem;
+  font-size: 1.5rem;
+  border-radius: 0.7rem;
+  background-color: #fff;
+  border: 1px solid #ffb4b4;
+  color: #555;
+  margin: 5rem auto;
+  &:hover {
+    background-color: #ffb4b4;
+    color: #fff;
+  }
+`;
+
+const TodoContainer = styled.div`
+  width: 100vw;
+`;
+const TodoUl = styled.ul`
+  width: 15rem;
+  margin: ${(props) => (props.isClicked ? "0 calc(35vw - 7.5rem)" : "auto")};
+`;
+const TodoLi = styled.li`
+  font-size: 1.5rem;
+  padding: 1rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #ffb4b4;
+    color: #fff;
+  }
+  background-color: ${(props) =>
+    props.id === props.value ? "#ffb4b4" : "#fff"};
+  color: ${(props) => (props.id === props.value ? "#fff" : "#555")};
+`;
