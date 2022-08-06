488ebe003a7852b61ec99db513e4a5e295acccbf
sojeongsojeong-park <psj90972@naver.com>
2022-08-06 13:13:21 +0900

    style:add signup page style

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function SignUp() {
   const [idInput, setIdInput] = useState("");
@@ -48,22 +49,59 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <input
-        type='text'
-        placeholder='email'
-        value={idInput}
-        onChange={idChangeHandler}
-      />
-      <input
-        type='password'
-        placeholder='password'
-        value={passwordInput}
-        onChange={passwordChangeHandler}
-      />
-      <button onClick={signupClickHandler}>SignUp</button>
-    </div>
+    <LoginContainer>
+      <LoginBox>
+        <Input
+          type='text'
+          placeholder='email'
+          value={idInput}
+          onChange={idChangeHandler}
+        />
+        <Input
+          type='password'
+          placeholder='password'
+          value={passwordInput}
+          onChange={passwordChangeHandler}
+        />
+        <SignupButton onClick={signupClickHandler}>SignUp</SignupButton>
+      </LoginBox>
+    </LoginContainer>
   );
 }
 
 export default SignUp;
+
+const LoginContainer = styled.div`
+  width: 100vw;
+`;
+const LoginBox = styled.div`
+  width: 30rem;
+  height: 20rem;
+  margin: 5rem auto;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 20rem;
+  height: 2rem;
+  border: none;
+  border-bottom: 1px solid #ffb4b4;
+  margin: 2rem auto;
+  outline: none;
+`;
+const SignupButton = styled.button`
+  width: 6rem;
+  height: 6rem;
+  font-size: 1.5rem;
+  color: #555;
+  background-color: #fff;
+  border: 1px solid #ffb4b4;
+  position: relative;
+  top: -8rem;
+  left: 27rem;
+  border-radius: 0.7rem;
+  &:hover {
+    color: #fff;
+    background-color: #ffb4b4;
+  }
+`;
